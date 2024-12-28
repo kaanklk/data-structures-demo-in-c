292d99e8d7273bca84cc19c7ee447b0c9362fc49
Kaan Kulak <kaankulak00@gmail.com>
2024-12-28 22:04:52 +0300

    Improved stack implementation

--- stack.c
+++ stack.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
-{
+struct node {
     int value;
     struct node *prev;
 };
@@ -10,9 +9,18 @@ struct node
 typedef struct node Node;
 typedef Node *NodePtr;
 
+struct stack {
+    Node *head;
+};
+
+typedef struct stack Stack;
+typedef Stack *StackPtr;
+
 /* Function protoypes */
-int push(int value, NodePtr *headNode);
-int pop(NodePtr *headNode);
+StackPtr initStack();
+int push(int value, StackPtr stackPtr);
+int pop(StackPtr stackPtr);
+void destroyStack(StackPtr StackPtr);
 
 int main(int argc, char const *argv[])
 {
@@ -21,27 +29,41 @@ int main(int argc, char const *argv[])
         effectively moving "up" the stack. When an element is popped, headNode is updated 
         to point to the previous node, moving "down" the stack.
     */
-    NodePtr headNode = NULL;
+    StackPtr sampleStack = initStack();
 
-    push(5,&headNode);
-    printf("Head node value: %d\n", headNode->value);
+    push(5, sampleStack);
+    printf("Head node value: %d\n", sampleStack->head->value);
     
-    push(12,&headNode);
-    printf("Head node value: %d\n", headNode->value);
+    push(12, sampleStack);
+    printf("Head node value: %d\n", sampleStack->head->value);
 
-    push(88,&headNode);
-    printf("Head node value: %d\n", headNode->value);
+    push(88, sampleStack);
+    printf("Head node value: %d\n", sampleStack->head->value);
 
-    push(42,&headNode);
-    printf("Head node value: %d\n", headNode->value);
+    push(42, sampleStack);
+    printf("Head node value: %d\n", sampleStack->head->value);
     
-    pop(&headNode);
-    printf("Head node value: %d\n", headNode->value);
+    pop(sampleStack);
+    printf("Head node value: %d\n", sampleStack->head->value);
+
+    destroyStack(sampleStack);
 
     return 0;
 }
 
-int push(int value, NodePtr *headNode) {
+StackPtr initStack() {
+    StackPtr newStack = malloc(sizeof(Stack));
+    if (newStack == NULL)
+    {
+        puts("Memory allocation failed!");
+        return NULL;
+    }
+    
+    newStack->head = NULL;
+    return newStack;
+}
+
+int push(int value, StackPtr stackPtr) {
     NodePtr newNode = malloc(sizeof(Node));
     if (newNode == NULL)
     {
@@ -50,24 +72,34 @@ int push(int value, NodePtr *headNode) {
     }
 
     newNode->value = value;
-    newNode->prev = *headNode;
-    *headNode = newNode;
+    newNode->prev = stackPtr->head;
+    stackPtr->head = newNode;
 
     return 0;
 }
 
-int pop(NodePtr *headNode) {
-    if (*headNode == NULL)
+int pop(StackPtr stackPtr) {
+    if (stackPtr->head == NULL)
     {
         puts("List is empty!");
         return -1;
     }
 
-    int poppedValue = (*headNode)->value;
-    NodePtr prevNode = (*headNode)->prev;
-    free(*headNode);
-    *headNode = prevNode;
+    int poppedValue = stackPtr->head->value;
+    NodePtr prevNode = stackPtr->head->prev;
+    free(stackPtr->head);
+    stackPtr->head = prevNode;
     return 0;
+}
+
+void destroyStack(StackPtr stackPtr) {
+    if (!stackPtr) return;
+    while (stackPtr->head != NULL) {
+        NodePtr temp = stackPtr->head;
+        stackPtr->head = stackPtr->head->prev;
+        free(temp);
+    }
+    free(stackPtr);
 }
 
 
